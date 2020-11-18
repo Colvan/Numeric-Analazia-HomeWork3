b4a515bb2eebed13adf63cd951d62ad390084160
colvan <yoniborisenco@gmail.com>
2020-11-18 16:47:56 +0200

    fixed
    fixed runs till iter 25

--- homework3.py
+++ homework3.py
@@ -1,60 +1,11 @@
-import math
-from numpy import linalg as LA
-
-# A matrix
-# b matrix
-# x solution
-# n size
-
-# jacob methdo
-
-
-def jacobiMethod(A, b, x, n):
-    itr = 0
-    tol = 1e-5
-    normVal = math.inf
-    while(normVal > tol):
-        xold = x
-        for i in range(n):
-            sigma = 0
-            for j in range(n):
-                if(i != j):
-                    sigma = sigma+A[i, j]*x[j]
-            x[i] = (1/A[i, i])*(b[i]-sigma)
-        itr = itr+1
-        normVal = abs(xold-x)
-    print('Solution of the system is : \n%f\n%f\n%f\n%f in %d iterations', x, itr)
-
-# A matrix
-# b matrix
-# x solution
-# n size
-
-# gauss method
-
-
-def gaussMethod(A, b, x, n):
-    itr = 0
-    tol = 1e-5
-    normVal = math.inf
-    while(normVal > tol):
-        x_old = x
-        for i in range(n):
-            sigma = 0
-            for j in range(i-1):
-                sigma = sigma+A[i, j]*x[j]
-            for j in range(i+1, n):
-                sigma = sigma+A[i, j]*x_old[j]
-            x[i] = (1/A[i, i])*(b[i]-sigma)
-        itr = itr+1
-        normVal = LA.norm(x_old, x)
-    print('Solution of the system is : \n%f\n%f\n%f\n%f in %d iterations', x, itr)
 
 
 # m=matrix
 # n=number of rows and columns
 
 # if matrix diagonal is dominant
+
+
 def isDominant(m, n):
     for i in range(0, n):
         sum = 0
@@ -62,5 +13,62 @@ def isDominant(m, n):
             sum = sum + abs(m[i][j])
         sum = sum - abs(m[i][i])
         if (abs(m[i][i]) < sum):
-            print("is not dominant")
-    print("is dominant")
+            return False
+    return True
+
+
+# A matrix
+# b matrix
+# x solution
+
+# jacob methdod
+
+
+def jacobiMethod(A, b, x):
+    itr = 0
+    check = False
+    n = len(A)
+    check = isDominant(A, n)
+    if(check):
+        while(itr < 25):
+            xold = x
+            for i in range(n):
+                sigma = 0
+                for j in range(n):
+                    if(i != j):
+                        sigma = sigma+A[i][j]*x[j]
+                x[i] = (1/A[i][i])*(b[i]-sigma)
+                print('current :', x)
+            itr = itr+1
+        print('Solution of the system is :', x)
+    else:
+        print('matrix diagonal not dominant')
+
+# A matrix
+# b matrix
+# x solution
+
+
+# gauss method
+
+
+def gaussMethod(A, b, x):
+    itr = 0
+    check = False
+    n = len(A)
+    check = isDominant(A, n)
+    if(check):
+        while(itr < 25):
+            x_old = x
+            for i in range(n):
+                sigma = 0
+                for j in range(i-1):
+                    sigma = sigma+A[i][j]*x[j]
+                for j in range(i+1, n):
+                    sigma = sigma+A[i][j]*x_old[j]
+                x[i] = (1/A[i][i])*(b[i]-sigma)
+                print('current :', x)
+            itr = itr+1
+        print('Solution of the system is :', x)
+    else:
+        print('matrix diagonal not dominant')
---
+++ test.py
@@ -0,0 +1,10 @@
+from homework3 import *
+
+
+x = [0, 0, 0]
+A = [[4, 1, 2], [3, 5, 1], [1, 1, 3], ]
+b = [4, 7, 3]
+
+
+jacobiMethod(A, b, x)
+gaussMethod(A, b, x)
