6d4b27d33ea50bd871006ecbda2024c821af8b1e
colvan <yoniborisenco@gmail.com>
2020-11-26 14:17:16 +0200

    updated epsilon

--- homework3.py
+++ homework3.py
@@ -29,18 +29,22 @@ def jacobiMethod(A, b, x):
     check = False
     n = len(A)
     check = isDominant(A, n)
+    epsilon = 0.001
+    stop = True
     if(check):
-        while(itr < 25):
-            xold = x
+        while(stop):
+            xold = x.copy()
             for i in range(n):
                 sigma = 0
                 for j in range(n):
                     if(i != j):
-                        sigma = sigma+A[i][j]*x[j]
+                        sigma = sigma+A[i][j]*xold[j]
                 x[i] = (1/A[i][i])*(b[i]-sigma)
                 print('current :', x)
             itr = itr+1
-        print('Solution of the system is :', x)
+            if(abs((x[0]-xold[0])) < epsilon):
+                stop = False
+        print('Solution of the system is :', x, itr)
     else:
         print('matrix diagonal not dominant')
 
@@ -56,9 +60,12 @@ def gaussMethod(A, b, x):
     itr = 0
     check = False
     n = len(A)
+    epsilon = 0.001
+    stop = True
     check = isDominant(A, n)
     if(check):
-        while(itr < 25):
+        while(stop):
+            xold = x.copy()
             for i in range(0, n):
                 sigma = b[i]
                 for j in range(0, n):
@@ -67,6 +74,8 @@ def gaussMethod(A, b, x):
                 x[i] = sigma/A[i][i]
                 print('current :', x)
             itr = itr+1
-        print('Solution of the system is :', x)
+            if(abs((x[0]-xold[0])) < epsilon):
+                stop = False
+        print('Solution of the system is :', x, itr)
     else:
         print('matrix diagonal not dominant')
