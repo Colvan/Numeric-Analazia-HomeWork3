00e48ddfbf325966cc2158785c958f28bebb72a8
colvan <yoniborisenco@gmail.com>
2020-12-07 15:31:39 +0200

    1

--- homework4.py
+++ homework4.py
@@ -56,3 +56,17 @@ def AllRangeCheck(aLeft, bRight, func, der):
                     print("The value of root is : ", "%f" % root_check[i])
         aLeft += run
         aLeft = round(aLeft, 1)
+
+
+def func(x):
+    return (x**4)+(x**3)-(3*(x**2))
+
+
+def der(x):
+    return (4*(x**3))+(3*(x**2))-6*x
+
+
+a = -3
+b = 2
+
+AllRangeCheck(a, b, func, der)
